427713964f5e8d8d7a0e22a78c109590275f6ec5
cruzach <charliecruzan@gmail.com>
2020-10-20 15:25:16 -0400

    first

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={require("./assets/one.jpeg")}
+        />
+
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={require("./assets/two.jpeg")}
+        />
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={require("./assets/three.jpeg")}
+        />
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={require("./assets/four.jpeg")}
+        />
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={require("./assets/five.jpeg")}
+        />
+        <Image
+          style={{ width: 50, height: 50 }}
+          source={require("./assets/six.jpeg")}
+        />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
